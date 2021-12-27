5c1f290f8dc38e0d97612d3ee47c77ef77cfcaa9
[Md Mahbub Rahman] <[mahbubrmn99@gmail.com]>
2021-12-27 14:47:10 +0600

    add cpp file

---
+++ 2.cpp
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main()
+{
+    printf("Today is Monday");
+    return 0;
+}
---
+++ 3.cpp
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main()
+{
+    printf("This is software Lab");
+    return 0;
+}
